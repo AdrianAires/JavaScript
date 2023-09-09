a6933878dda501bf5f60cd9a38be03fef59df19d
AdrianAires <107860137+AdrianAires@users.noreply.github.com>
2023-09-09 20:51:01 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,34 @@
-document.querySelector('.tecla_poms')
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
 
-document.querySelector('#som_tecla_pom').play();
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocaSom(idAudio)
+    }
+
+    tecla.onkeydown = function (evento) {
+
+        console.log(evento.code == 'Space')
+
+        if (evento.code === 'Space'){
+        tecla.classList.add('ativa');
+        }
+
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
+}
